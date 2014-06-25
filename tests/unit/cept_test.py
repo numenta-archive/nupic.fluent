26c78d8cf59149153b0a77b9f60423ebe923ff71
Viraj Sinha <vsinha@numenta.com>
2014-06-25 15:30:16 -0700

    mocked API key check test

---
+++ tests/unit/cept_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+import unittest
+from mock import patch
+
+from fluent.cept import Cept
+
+class TestCept(unittest.TestCase):
+
+  @patch('os.environ')
+  def testCheckForApiKey(self, mockOS):
+    with self.assertRaises(Exception) as e:
+      cept = Cept()
+    self.assertIn("Missing API key.", e.exception)
+
+if __name__ == '__main__':
+  unittest.main()
