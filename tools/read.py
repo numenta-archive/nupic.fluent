108d3797d71d095ca6e3f6cc241f0dfb45833fbd
Viraj Sinha <vsinha@numenta.com>
2014-06-24 17:23:40 -0700

    merge with master

--- tools/read.py
+++ tools/read.py
@@ -26,7 +26,7 @@ from fluent.model import Model
 from fluent.term import Term
 
 
-def readFile(filename, model, resetSequences=False):
+def readFile(filename, model, resetSequences=False, format=None):
   if model.canCheckpoint() and model.hasCheckpoint():
     model.load()
 
@@ -85,6 +85,12 @@ if __name__ == '__main__':
       dest="checkpoint",
       help="Directory to save model to and load model from")
   parser.add_option(
+      "-f",
+      "--format",
+      dest="format",
+      help="Format to output (ie: csv)",
+      metavar=FORMAT)
+  parser.add_option(
       "-r",
       "--reset-sequences",
       dest="resetSequences",
@@ -103,7 +109,7 @@ if __name__ == '__main__':
 
   try:
     readFile(args[0], model,
-             resetSequences=options.resetSequences)
+             resetSequences=options.resetSequences, format=options.format)
   except KeyboardInterrupt:
     if model.canCheckpoint():
       print("Saving model before exiting...")
